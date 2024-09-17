47802ea61814564044df9c9074ab4f2bbec8a732
william0228 <songyunwus@gmail.com>
2024-09-17 00:18:45 -0700

    Initial Commit

---
+++ Purchase App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Purchase App
+//
+//  Created by 王嵩允 on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

082dc2429cd2c910ad3f6876c586257dea9fcf8e
william0228 <songyunwus@gmail.com>
2024-09-17 02:58:59 -0700

    - Setup the storeKit configuration - Create the simple storyboard for the app - Make a function when user want to do in-app purchase

---
+++ Purchase App/QuoteTableViewController.swift
@@ -0,0 +1,96 @@
+//
+//  QuoteTableViewController.swift
+//  Purchase App
+//
+//  Created by 王嵩允 on 9/17/24.
+//
+
+import UIKit
+import StoreKit
+
+class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
+    
+    let productID = "com.songyunwang.purchaseApp.premiumQuotes"
+    
+    var quotesToShow = [
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f"
+    ]
+    
+    let premiumQuotes = [
+        "Thanks",
+        "Thanks",
+        "Thanks",
+        "Thanks",
+        "Thanks",
+        "Thanks"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        SKPaymentQueue.default().add(self)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quotesToShow.count + 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        if indexPath.row < quotesToShow.count {
+            cell.textLabel?.text = quotesToShow[indexPath.row]
+            cell.textLabel?.numberOfLines = 0
+        } else {
+            cell.textLabel?.text = "Get More Quotes..."
+            cell.textLabel?.textColor = UIColor(red: 0.61, green: 0.53, blue: 1.00, alpha: 1.00)
+            cell.accessoryType = .disclosureIndicator
+        }
+        return cell
+    }
+    
+    // MARK: - Table view delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == quotesToShow.count {
+            buyPremiumQuotes()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - In-App Purchase Methods
+    func buyPremiumQuotes() {
+        // Check User can make payment
+        if SKPaymentQueue.canMakePayments() {
+            let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(paymentRequest)
+        } else {
+            print("User can't make payment!")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            if transaction.transactionState == .purchased {
+                print("Success")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            } else if transaction.transactionState == .failed {
+                if let error = transaction.error {
+                    let errorDescription = error.localizedDescription
+                    print("Transaction failed due to error: \(errorDescription)")
+                }
+                SKPaymentQueue.default().finishTransaction(transaction)
+            }
+        }
+    }
+
+    @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+    }
+}
